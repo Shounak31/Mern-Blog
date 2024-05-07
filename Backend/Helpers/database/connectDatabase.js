d7ae6f3ab779d5a3249f290492276e88ee7e2ca1
Shounak Deshmukh <deshmukhshounak07@gmail.com>
2024-05-07 23:59:32 +0530

    update db connection from mongo atlas to compass

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose")
 
 const connectDatabase =async  () => {
 
-    await mongoose.connect('mongodb+srv://deshmukhshounak07:nCYIaEjUpFiOYbjd@cluster0.wkgkwt2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ,{useNewUrlParser : true})
+   // await mongoose.connect('mongodb+srv://deshmukhshounak07:nCYIaEjUpFiOYbjd@cluster0.wkgkwt2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ,{useNewUrlParser : true})
+
+   await mongoose.connect('mongodb://localhost:27017/mern-blog' ,{useNewUrlParser : true})
 
     console.log("MongoDB Connection Successfully")
 
