552a043c124727f7a4c75da805f64b7566961503
Shounak Deshmukh <deshmukhshounak07@gmail.com>
2024-05-08 00:12:01 +0530

    update config dotenv file

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -5,7 +5,7 @@ const connectDatabase =async  () => {
 
    // await mongoose.connect('mongodb+srv://deshmukhshounak07:nCYIaEjUpFiOYbjd@cluster0.wkgkwt2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ,{useNewUrlParser : true})
 
-   await mongoose.connect('mongodb://localhost:27017/mern-blog' ,{useNewUrlParser : true})
+   await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
 
     console.log("MongoDB Connection Successfully")
 
